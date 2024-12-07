ad5bfe2dcc376904c8675ae2665a10c381a64ebe
edy alves <30.edyalves@gmail.com>
2024-12-07 18:09:29 -0300

    Adcionando funcionalidade de navegacao e armazenamento local.

---
+++ UteisUIKit/DetalisView.swift
@@ -0,0 +1,15 @@
+//
+//  Detalis.swift
+//  UteisUIKit
+//
+//  Created by ednardo alves on 07/12/24.
+//
+
+import Foundation
+import SwiftUI
+
+public struct DetalisView: View {
+    public var body: some View {
+        Text("Detalis")
+    }
+}
---
+++ UteisUIKit/Global.swift
@@ -0,0 +1,8 @@
+//
+//  Global.swift
+//  UteisUIKit
+//
+//  Created by ednardo alves on 07/12/24.
+//
+
+import Foundation
